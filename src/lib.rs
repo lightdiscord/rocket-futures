58a7a6b7f2517a34866a8d3082441ebc3846de98
LightDiscord <root@arnaud.sh>
2018-11-07 18:36:00 +0100

    Implement Responder trait on wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate rocket;
+extern crate futures;
+extern crate tokio;
+
+use rocket::response::{ Responder, Response };
+use rocket::http::Status;
+use rocket::Request;
+use futures::Future;
+use tokio::runtime::Runtime;
+use std::fmt::Debug;
+
+pub struct Wrapper<T>(T);
+
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
+        Wrapper(value)
+    }
+}
+
+impl<'r, T> Responder<'r> for Wrapper<T>
+    where T: Future + Send + 'static,
+          T::Item: Send + Responder<'r> + 'static,
+          T::Error: Send + Debug + 'static {
+
+    fn respond_to(self, req: &Request) -> Result<Response<'r>, Status> {
+        Runtime::new().unwrap().block_on(self.0).respond_to(req)
     }
 }
